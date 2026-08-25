b319be33f0de56a2a690028a15537e65a9053683
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-08-25 19:50:30 -0300

    fix: removido geracao de UUID automatico

--- src/main/java/com/mattheusfernandes/right/time/backend/domain/entity/Customer.java
+++ src/main/java/com/mattheusfernandes/right/time/backend/domain/entity/Customer.java
@@ -15,7 +15,6 @@ import java.util.UUID;
 public class Customer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @Column(name = "name", nullable = false)
