2e02b278b8005ee151d7a3e7fbb15e62bb6cdbe3
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-08-26 10:02:44 -0300

    docs: documenta endpoint de criação de cliente com Swagger

--- src/main/java/com/mattheusfernandes/right/time/backend/rest/CustomerResource.java
+++ src/main/java/com/mattheusfernandes/right/time/backend/rest/CustomerResource.java
@@ -2,6 +2,8 @@ package com.mattheusfernandes.right.time.backend.rest;
 
 import com.mattheusfernandes.right.time.backend.domain.service.CustomerService;
 import com.mattheusfernandes.right.time.backend.rest.dto.CreateCustomerRequest;
+import com.mattheusfernandes.right.time.backend.rest.dto.CustomerResponse;
+import com.mattheusfernandes.right.time.backend.rest.dto.ErrorMessage;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
@@ -9,6 +11,10 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 
 @Path("/customers")
 @Produces(MediaType.APPLICATION_JSON)
@@ -19,6 +25,27 @@ public class CustomerResource {
     private CustomerService customerService;
 
     @POST
+    @Operation(
+            summary = "Criar cliente",
+            description = "Criar um novo cliente"
+    )
+    @APIResponse(
+            responseCode = "201",
+            description = "Cliente cadastrado com sucesso",
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON,
+                    schema = @Schema(implementation = CustomerResponse.class)
+            )
+    )
+    @APIResponse(
+            responseCode = "409",
+            description = "Cliente ou celular já cadastrado",
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON,
+                    schema = @Schema(implementation = ErrorMessage.class)
+            )
+
+    )
     public Response createCustomer(CreateCustomerRequest request) {
 
         return Response
