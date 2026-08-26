25b2e695fb36eb94e936c43557f92cba24131a6a
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-08-26 10:00:45 -0300

    feature: adicionado @schema para swagger

--- src/main/java/com/mattheusfernandes/right/time/backend/rest/dto/CustomerResponse.java
+++ src/main/java/com/mattheusfernandes/right/time/backend/rest/dto/CustomerResponse.java
@@ -1,13 +1,33 @@
 package com.mattheusfernandes.right.time.backend.rest.dto;
 
 import lombok.Builder;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 import java.util.UUID;
 
+@Schema(description = "Dados do cliente")
 @Builder
 public record CustomerResponse(
+        @Schema(
+                description = "Identificador único do cliente",
+                example = "Mattheus"
+        )
         UUID id,
+        @Schema(
+                description = "Nome do cliente",
+                example = "Mattheus"
+        )
         String name,
+
+        @Schema(
+                description = "Nome do cliente",
+                example = "Fernandes"
+        )
         String lastname,
+
+        @Schema(
+                description = "Nome do cliente",
+                example = "00000000000"
+        )
         String phone
 ) { }
--- src/main/java/com/mattheusfernandes/right/time/backend/rest/dto/ErrorMessage.java
+++ src/main/java/com/mattheusfernandes/right/time/backend/rest/dto/ErrorMessage.java
@@ -1,6 +1,20 @@
 package com.mattheusfernandes.right.time.backend.rest.dto;
 
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+@Schema(description = "Resposta erro da API")
 public record ErrorMessage(
+
+        @Schema(
+                description = "Código HTTP do erro",
+                example = "409"
+        )
+        int status,
+
+        @Schema(
+                description = "Mensagem do erro",
+                example = "Cliente já cadastrado"
+        )
         String message
 ) {
 }
