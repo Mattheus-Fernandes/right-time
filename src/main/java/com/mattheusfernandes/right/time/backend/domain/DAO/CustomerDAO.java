3069d130b3e0ba6430ce5cf21016e2ab3b4c75e4
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-08-26 09:35:56 -0300

    feature: adicionado DAO para buscar de cliente e celular existentes

--- src/main/java/com/mattheusfernandes/right/time/backend/domain/DAO/CustomerDAO.java
+++ src/main/java/com/mattheusfernandes/right/time/backend/domain/DAO/CustomerDAO.java
@@ -5,7 +5,6 @@ import com.mattheusfernandes.right.time.backend.utils.query.CustomerQuery;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
-import jakarta.transaction.Transactional;
 
 import java.util.UUID;
 
@@ -30,5 +29,26 @@ public class CustomerDAO {
         customer.setId(id);
 
         return customer;
+    }
+
+    public boolean existsByFullname(String name, String lastname) {
+
+        Number count = (Number) entityManager
+                .createNamedQuery(CustomerQuery.EXISTS_BY_FULLNAME)
+                .setParameter("name", name)
+                .setParameter("lastname", lastname)
+                .getSingleResult();
+
+        return count.longValue() > 0;
+    }
+
+    public boolean existsByPhone(String phone) {
+
+        Number count = (Number) entityManager
+                .createNamedQuery(CustomerQuery.EXISTS_BY_PHONE)
+                .setParameter("phone", phone)
+                .getSingleResult();
+
+        return count.longValue() > 0;
     }
 }
