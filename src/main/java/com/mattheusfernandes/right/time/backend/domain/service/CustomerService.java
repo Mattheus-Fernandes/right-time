ed1e4b37ec084a316dec087c7482b740601946d4
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-08-26 10:55:49 -0300

    feature: adicionado validacao de campos

--- src/main/java/com/mattheusfernandes/right/time/backend/domain/service/CustomerService.java
+++ src/main/java/com/mattheusfernandes/right/time/backend/domain/service/CustomerService.java
@@ -4,6 +4,7 @@ import com.mattheusfernandes.right.time.backend.domain.entity.Customer;
 import com.mattheusfernandes.right.time.backend.domain.mapper.CustomerMapper;
 import com.mattheusfernandes.right.time.backend.domain.repository.CustomerRepositoryImp;
 import com.mattheusfernandes.right.time.backend.exceptions.CustomerAlreadyExistsException;
+import com.mattheusfernandes.right.time.backend.exceptions.CustomerInvalidException;
 import com.mattheusfernandes.right.time.backend.rest.dto.CreateCustomerRequest;
 import com.mattheusfernandes.right.time.backend.rest.dto.CustomerResponse;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -19,6 +20,7 @@ public class CustomerService {
     @Transactional
     public CustomerResponse saveCustomer(CreateCustomerRequest request) {
 
+        validateCustomer(request);
         verifyCustomerFullname(request.name(), request.lastname());
         verifyCustomerPhone(request.phone());
 
@@ -38,6 +40,24 @@ public class CustomerService {
     private void verifyCustomerPhone(String phone) {
         if(customerRepository.existsByPhone(phone)) {
             throw new CustomerAlreadyExistsException("Número de celular já cadastrado");
+        }
+    }
+
+    private void validateCustomer(CreateCustomerRequest request) {
+        if (request.name() == null || request.name().isBlank()) {
+            throw new CustomerInvalidException("Nome é obrigatório");
+        }
+
+        if (request.lastname() == null || request.lastname().isBlank()) {
+            throw new CustomerInvalidException("Sobrenome é obrigatório");
+        }
+
+        if (request.phone() == null || request.phone().isBlank()) {
+            throw new CustomerInvalidException("Número de celular é obrigatório");
+        }
+
+        if (!request.phone().matches("\\d{11}")) {
+            throw new CustomerInvalidException("Número de celular deve possuir 11 dígitos");
         }
     }
 }
