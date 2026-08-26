99a97624f6c340e24ccbcea9071b882e01d5fb2e
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-08-26 09:38:50 -0300

    feature: adicionado execeptions customizada para cliente

---
+++ src/main/java/com/mattheusfernandes/right/time/backend/exceptions/CustomerAlreadyExistsExceptionMapper.java
@@ -0,0 +1,19 @@
+package com.mattheusfernandes.right.time.backend.exceptions;
+
+import com.mattheusfernandes.right.time.backend.rest.dto.ErrorMessage;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class CustomerAlreadyExistsExceptionMapper implements ExceptionMapper<CustomerAlreadyExistsException> {
+
+    @Override
+    public Response toResponse(CustomerAlreadyExistsException exception) {
+
+        return Response
+                .status(Response.Status.CONFLICT)
+                .entity(new ErrorMessage(exception.getMessage()))
+                .build();
+    }
+}
