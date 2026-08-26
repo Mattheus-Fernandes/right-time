ea91fad301761e6df5d632035f8e3c4597dea76e
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-08-26 09:38:21 -0300

    feature: adicionado novas queries

--- src/main/java/com/mattheusfernandes/right/time/backend/utils/query/CustomerQuery.java
+++ src/main/java/com/mattheusfernandes/right/time/backend/utils/query/CustomerQuery.java
@@ -19,4 +19,20 @@ public class CustomerQuery {
             )
             """;
 
+    public static final String EXISTS_BY_FULLNAME = "Customer.existsByFullname";
+
+    public static final String EXISTS_BY_FULLNAME_QUERY = """
+            SELECT COUNT(*)
+            FROM Customers
+            WHERE LOWER(name) = LOWER(:name)
+            AND LOWER(lastname) = LOWER(:lastname)
+            """;
+    public static final String EXISTS_BY_PHONE =
+            "Customer.existsByPhone";
+
+    public static final String EXISTS_BY_PHONE_QUERY = """
+            SELECT COUNT(*)
+            FROM customers
+            WHERE phone = :phone
+            """;
 }
